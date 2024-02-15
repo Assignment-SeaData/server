258f2b914016f288c64ec265b1993440b0a278eb
Art96em <artem-w0lf@yandex.ru>
2024-02-15 12:37:55 +0200

    pass

--- service/service.mjs
+++ service/service.mjs
@@ -1,4 +1,4 @@
 import SQLConnection from "../domain/SQLConnection.mjs";
 import UserService from "./UserService.mjs";
 
-export const service = new UserService(new SQLConnection('localhost', 'root', 'admin1234', 'exam'))
+export const service = new UserService(new SQLConnection('localhost', 'root', 'artar', 'exam'))
